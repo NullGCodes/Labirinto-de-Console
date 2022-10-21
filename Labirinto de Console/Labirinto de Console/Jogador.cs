7f1c5fc4070cc49f6e72e26041fcba2079d52ba6
NullGCodes <116277784+NullGCodes@users.noreply.github.com>
2022-10-20 22:51:09 -0300

    Add files via upload

---
+++ Labirinto de Console/Labirinto de Console/Jogador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labirinto
+{
+    class Jogador
+    {
+        public void Mover(int X, int Y)
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write("O");
+
+
+            System.Threading.Thread.Sleep(50);
+            Console.Clear();
+            
+
+        }
+        public Jogador()
+        { }
+    }
+}
