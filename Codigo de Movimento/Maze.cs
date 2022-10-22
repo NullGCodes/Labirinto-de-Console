797a6f41cd7e60e703b6973954de107b22c17ad9
NullGCodes <116277784+NullGCodes@users.noreply.github.com>
2022-10-22 12:07:29 -0300

    Reuploading the fixed version
    needed to fix a naming mistake

---
+++ Codigo de Movimento/Maze.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labirinto
+{
+    class Maze
+    {
+
+        public string[] maze
+        {
+            get { return L; }
+        }
+        int y = 0;
+        
+
+        string[] L = {             "───────────────────┬────────────────────┐\n" +
+                                   "                   │                    │\n"+
+                                   "┌───────────────┐  │  ┌────────────┐    │\n"+
+                                   "│               │  │  │                 │\n"+
+                                   "│  ┌──┐  ┌───┐  │  │  │   ──────────────┤\n"+
+                                   "│  │  │  │      │  │  │                 │\n"+
+                                   "│  │  │  └──────┘  │  └──────────────┐  │\n"+
+                                   "│     │            │                 │  │\n"+
+                                   "│  │  └────────────┤  ┌──────   ┌────┘  │\n"+
+                                   "│  │               │  │         │       │\n"+
+                                   "│  └────────────┐  ┴  │     │   │    │  │\n"+
+                                   "│               │     │     │   │    │  │\n"+
+                                   "├───────────────┼─────┘     │   │    │  │\n"+
+                                   "│               │           │   │    │  │\n"+
+                                   "├─────    ──────┤  │   ┌────┘   │    │  │\n"+
+                                   "│                  │   │        │    │  │\n"+
+                                   "│───────────────┬──┴───┴──┬─┤   │    │  │\n"+
+                                   "│               │         │     │    │  │\n"+
+                                   "│  │    ────┐   │   ──┐   │   ├─┘    │  │\n"+
+                                   "│  │        │   │     │   │          │  │\n"+
+                                   "│  └─────┐  └───┴──   │   └──────────┘  │\n"+
+                                   "         │            │                 │\n"+
+                                   "─────────┴────────────┴─────────────────┘"};
+        public char[] Fragmentar()
+        {
+            char[] c = String.Join(string.Empty, L).ToCharArray();
+            return c;
+        }
+        
+        public void Imprimir()
+        {
+            
+
+            for (int i = 0; i < L.Length; i++)
+            {
+
+                Console.SetCursorPosition(0, i);
+                string C = L[i];
+                Console.Write(C);
+                y=i;
+
+            }
+
+        }
+        public double Y
+        {
+            get { return y; }
+        }
+        
+        public Maze()
+        {
+        }
+    }
+}
+
